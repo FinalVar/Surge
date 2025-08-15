9b09f31179d1224fb60bebd63bf359284c01241d
FinalVar <126796550+FinalVar@users.noreply.github.com>
2025-08-15 16:33:02 +0800

    Update notability.js

--- notability.js
+++ notability.js
@@ -1,18 +1,5 @@
-/**
- * notability解锁plus，如果解锁无效果，点击恢复计划
- * 公众号：木瞳科技Pro
- *
- * [MITM]
- * hostname = notability.com
- * 
- * 
- * 
- */
-
 const SCRIPT_NAME = 'Notability';
-
 const plus = /^https?:\/\/notability\.com\/(global|subscriptions)/;
-
 if(plus.test($request.url)) {
     let obj = JSON.parse($response.body);
     obj.data = {
